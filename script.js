3a42797fb1ad0a047e8bf2839438e621cd8d1761
Sony <sonychimo10@hotmail.com>
2026-05-10 19:58:23 -0600

    Actividad terminada

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-const librosLeidos = [];
+let librosLeidos = [];
 
+agregarLibro("Biblia");
+agregarLibro("Principito");
+agregarLibro("Minion")
+mostrarLibrosLeidos(librosLeidos);
 
 function agregarLibro(libro){
     librosLeidos.push(libro);
 }
 
 function mostrarLibrosLeidos(libros) {
-        
+    for (let libro of libros) { 
+        console.log("Libro leido: " + libro);
+    }
 }
